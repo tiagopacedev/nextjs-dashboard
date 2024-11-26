f5ed47dca5adefad3e25c6182ba2b5a573fdadd9
Tiago Pace <tiagopace.dev@gmail.com>
2024-11-26 16:19:39 -0300

    07 - Fetching Data

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -14,17 +14,14 @@ export default async function LatestInvoices({
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
                 key={invoice.id}
-                className={clsx(
-                  'flex flex-row items-center justify-between py-4',
-                  {
-                    'border-t': i !== 0,
-                  },
-                )}
+                className={clsx('flex flex-row items-center justify-between py-4', {
+                  'border-t': i !== 0,
+                })}
               >
                 <div className="flex items-center">
                   <Image
@@ -35,23 +32,17 @@ export default async function LatestInvoices({
                     height={32}
                   />
                   <div className="min-w-0">
-                    <p className="truncate text-sm font-semibold md:text-base">
-                      {invoice.name}
-                    </p>
-                    <p className="hidden text-sm text-gray-500 sm:block">
-                      {invoice.email}
-                    </p>
+                    <p className="truncate text-sm font-semibold md:text-base">{invoice.name}</p>
+                    <p className="hidden text-sm text-gray-500 sm:block">{invoice.email}</p>
                   </div>
                 </div>
-                <p
-                  className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
-                >
+                <p className={`${lusitana.className} truncate text-sm font-medium md:text-base`}>
                   {invoice.amount}
                 </p>
               </div>
-            );
+            )
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
