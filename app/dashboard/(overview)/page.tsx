c83f5775339d25b06afc1a4b4cf33d4dedd18eab
Tiago Pace <tiagopace.dev@gmail.com>
2024-11-28 16:54:56 -0300

    16 - Adding Metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,11 +6,13 @@ import { fetchCardData } from '@/app/lib/data'
 import { Suspense } from 'react'
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons'
 import CardWrapper from '@/app/ui/dashboard/cards'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
 
 export default async function Page() {
-  const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } =
-    await fetchCardData()
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
